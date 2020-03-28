580c733c2bb4238ea04c85f6992375be175bb282
Jingjing Duan <duanjingjing@gmail.com>
2020-03-28 12:49:28 -0700

    05_type_as_constraints.rs

--- jira-wip/src/koans/02_ticket_store/05_type_as_constraints.rs
+++ jira-wip/src/koans/02_ticket_store/05_type_as_constraints.rs
@@ -1,8 +1,8 @@
 mod type_as_constraints {
-    use std::collections::HashMap;
-    use chrono::{DateTime, Utc};
-    use super::recap::Status;
     use super::id_generation::TicketId;
+    use super::recap::Status;
+    use chrono::{DateTime, Utc};
+    use std::collections::HashMap;
 
     /// We know that id and creation time will never be there before a ticket is saved,
     /// while they will always be populated after `save` has been called.
@@ -36,12 +36,17 @@ mod type_as_constraints {
 
     #[derive(Debug, Clone, PartialEq)]
     pub struct TicketDraft {
-        __
+        title: String,
+        description: String,
     }
 
     #[derive(Debug, Clone, PartialEq)]
     pub struct Ticket {
-        __
+        id: TicketId,
+        title: String,
+        description: String,
+        created_at: DateTime<Utc>,
+        status: Status,
     }
 
     struct TicketStore {
@@ -50,16 +55,14 @@ mod type_as_constraints {
     }
 
     impl TicketStore {
-        pub fn new() -> TicketStore
-        {
+        pub fn new() -> TicketStore {
             TicketStore {
                 data: HashMap::new(),
                 current_id: 0,
             }
         }
 
-        pub fn save(&mut self, draft: TicketDraft) -> TicketId
-        {
+        pub fn save(&mut self, draft: TicketDraft) -> TicketId {
             let id = self.generate_id();
 
             // We can use the "raw" constructor for `Ticket` here because the
@@ -70,7 +73,11 @@ mod type_as_constraints {
             // This enforces our desired invariant: saving a draft in the store
             // is the only way to "create" a `Ticket`.
             let ticket = Ticket {
-
+                id,
+                title: draft.title,
+                description: draft.description,
+                created_at: Utc::now(),
+                status: Status::ToDo,
             };
             self.data.insert(id, ticket);
             id
@@ -87,16 +94,30 @@ mod type_as_constraints {
     }
 
     impl TicketDraft {
-        pub fn title(&self) -> &String { todo!() }
-        pub fn description(&self) -> &String { todo!() }
+        pub fn title(&self) -> &String {
+            todo!()
+        }
+        pub fn description(&self) -> &String {
+            todo!()
+        }
     }
 
     impl Ticket {
-        pub fn title(&self) -> &String { todo!() }
-        pub fn description(&self) -> &String { todo!() }
-        pub fn status(&self) -> &Status { todo!() }
-        pub fn created_at(&self) -> &DateTime<Utc> { todo!() }
-        pub fn id(&self) -> &TicketId { todo!() }
+        pub fn title(&self) -> &String {
+            &self.title
+        }
+        pub fn description(&self) -> &String {
+            &self.description
+        }
+        pub fn status(&self) -> &Status {
+            &self.status
+        }
+        pub fn created_at(&self) -> &DateTime<Utc> {
+            &self.created_at
+        }
+        pub fn id(&self) -> &TicketId {
+            &self.id
+        }
     }
 
     pub fn create_ticket_draft(title: String, description: String) -> TicketDraft {
@@ -110,20 +131,16 @@ mod type_as_constraints {
             panic!("A description cannot be longer than 3000 characters!");
         }
 
-        TicketDraft {
-            title,
-            description,
-        }
+        TicketDraft { title, description }
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
-        use fake::{Faker, Fake};
+        use fake::{Fake, Faker};
 
         #[test]
-        fn a_ticket_with_a_home()
-        {
+        fn a_ticket_with_a_home() {
             let draft = generate_ticket_draft();
             let mut store = TicketStore::new();
 
@@ -137,8 +154,7 @@ mod type_as_constraints {
         }
 
         #[test]
-        fn a_missing_ticket()
-        {
+        fn a_missing_ticket() {
             let ticket_store = TicketStore::new();
             let ticket_id = Faker.fake();
 
@@ -146,8 +162,7 @@ mod type_as_constraints {
         }
 
         #[test]
-        fn id_generation_is_monotonic()
-        {
+        fn id_generation_is_monotonic() {
             let n_tickets = 100;
             let mut store = TicketStore::new();
 
